adc618dce4fe465e304a1b98579dc229bac25ec7
Jared Chapman <jaredpchapman@gmail.com>
2016-10-01 11:07:51 -0600

    added more

--- mandelclean.ml
+++ mandelclean.ml
@@ -19,21 +19,25 @@ let getColor its = match its with
 let rec mandel m (x, y) (a, b) i = match (m, a*.a, b*.b, a*.b, i) with
 | (_,aa,bb,_,_) when (aa +. bb) > 4.0 -> i
 | (m,_,_,_,i) when i > m -> (-1)
-| (_,aa,bb,ab,_) -> mandel m (x, y) (aa -. bb +. x) (ab +. ab +. y) (i+1);;
+| (_,aa,bb,ab,_) -> mandel m (x, y) ((aa -. bb +. x), (ab +. ab +. y)) (i+1);;
 
-mandelcalc m (x, y) = mandel m (x,y) (x,y) 1;;
+let mandelcalc m (x, y) = mandel m (x,y) (x,y) 1;;
 
 let rec find_interesting m (x, y) = match (mandelcalc m (x,y) ) with
 | (i) when i > m - m/10 && i < m -> (x, y)
-| _ -> find_interesting m ((Random.float 4.0) -. 2.0) ((Random.float 4.0) -.2.0);;
+| _ -> find_interesting m (((Random.float 4.0) -. 2.0), ((Random.float 4.0) -.2.0));;
 
 let loop (xl, yl) (xh, yh) inc =
  let rec inner_loop x y acc = match (x, y) with
  | (_,y) when y > yh -> acc
  | (x,_) when x > xh -> inner_loop xl (y+.inc) acc
  | _ -> inner_loop (x+.inc) y ((x,y)::acc)
- in inner_loop xl yl [];
+ in inner_loop xl yl [];;
 
-let average_color colors =
-    let s cols = foldl () ((0,0,0), 0) cols
-    in s colors
+let average_color colors = function
+    |[] -> (0,0,0)
+    |xs ->
+      let sum cols =
+          foldl (fun ((ar,ag,ab),i) (r,g,b) -> ((ar+r,ag+g,ab+b),i+1) ) ((0,0,0), 0) cols in
+      let div ((a,b,c),n) = (a/n,b/n,c/n)
+      in div (sum xs);;
