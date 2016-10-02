bb9a5c12ca082cd1f0a7ae58f04cf13b2e7c6465
Jared Chapman <jaredpchapman@gmail.com>
2016-10-01 18:37:58 -0600

    new

--- mandelclean.ml
+++ mandelclean.ml
@@ -1,8 +1,12 @@
+(* Make stuff look nicer *)
 open Printf;;
 open List;;
 let foldl = fold_left;;
 let foldr = fold_right;;
 
+(* To go from R0 -> R1 in N steps
+ * R = (i*(R1-R0) / (N - 1)) + R0
+ *)
 let getGradient (r0, g0, b0) (r1, g1, b1) steps iters =
     match (steps, iters) with
     | (n, i) ->
@@ -10,22 +14,27 @@ let getGradient (r0, g0, b0) (r1, g1, b1) steps iters =
     int_of_float (float i *. (float (g1-g0) /. float (n-1))) + g0,
     int_of_float (float i *. (float (b1-b0) /. float (n-1))) + b0);;
 
+(* Choose gradients/bands of color *)
 let getColor its = match its with
 | (-1) -> (0,0,0)
 | x when x < 400 -> getGradient (0,100,255) (255,255,255) 400 x
 | x when x < 700 -> getGradient (255,255,255) (0,100,255) 300 ((x-400) mod 300)
 | x -> getGradient (0,100,255) (255,255,255) 300 ((x-700) mod 300);;
 
-let rec mandel m (x, y) (a, b) i = match (m, a*.a, b*.b, a*.b, i) with
+(* mandelbrot calculation *)
+let rec mandelcalc m (x, y) (a, b) i = match (m, a*.a, b*.b, a*.b, i) with
 | (_,aa,bb,_,_) when (aa +. bb) > 4.0 -> i
 | (m,_,_,_,i) when i > m -> (-1)
-| (_,aa,bb,ab,_) -> mandel m (x, y) ((aa -. bb +. x), (ab +. ab +. y)) (i+1);;
+| (_,aa,bb,ab,_) -> mandelcalc m (x, y) ((aa -. bb +. x), (ab +. ab +. y)) (i+1);;
 
-let mandelcalc m (x, y) = mandel m (x,y) (x,y) 1;;
+let mandel m (x, y) = mandelcalc m (x,y) (x,y) 1;;
 
-let rec find_interesting m (x, y) = match (mandelcalc m (x,y) ) with
-| (i) when i > m - m/10 && i < m -> (x, y)
-| _ -> find_interesting m (((Random.float 4.0) -. 2.0), ((Random.float 4.0) -.2.0));;
+let rec find_interesting m (x, y) = match (mandel m (x,y) ) with
+| (i) when i < m && (i > (m - m/10)) -> (x, y)
+| _ -> find_interesting m (((Random.float 4.0) -. 2.0), ((Random.float 4.0) -. 2.0));;
+
+let julia m (a,b) (x, y) =
+    mandelcalc m (a,b) (x,y) 1;;
 
 let loop (xl, yl) (xh, yh) inc =
  let rec inner_loop x y acc = match (x, y) with
@@ -41,3 +50,24 @@ let average_color colors = function
           foldl (fun ((ar,ag,ab),i) (r,g,b) -> ((ar+r,ag+g,ab+b),i+1) ) ((0,0,0), 0) cols in
       let div ((a,b,c),n) = (a/n,b/n,c/n)
       in div (sum xs);;
+
+let print_color c = match c with
+| (r,g,b) -> printf "%d %d %d" r g b;;
+
+
+let append_file fname s =
+    let out = open_out_gen [Open_wronly; Open_append; Open_creat; Open_text] 0o666 fname
+    in output_string out s;
+    close_out out;;
+
+let print_ppm xsize ysize = 
+    print_string "P3\n";
+    print_string ((string_of_int xsize) ^ " " ^ (string_of_int ysize) ^ "\n");
+print_string "255\n";
+    (*itermandel (float xsize) (float ysize) (find_interesting 2.0 2.0) ((Random.float 120.0)+.1000.0);*)
+    print_string "\n";;
+
+let () = 
+    Random.self_init ();
+    print_ppm 1920 1080;;
+    
